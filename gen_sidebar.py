fc7d5a31885bfd1a55c9824960eb912a7e667e6d
akira <chin.kou.akira@gmail.com>
2019-12-16 15:31:21 +0800

    generate TOC

--- gen_sidebar.py
+++ gen_sidebar.py
@@ -9,7 +9,7 @@ doc_path = "docs"
 read_me_file = "README.md"
 cname_file = "CNAME"
 
-ignore_files = [gen_file_name, ".git", read_me_file]
+ignore_files = [gen_file_name, ".git", read_me_file, "images"]
 
 
 def sorted_alphanumeric(data):
@@ -18,22 +18,31 @@ def sorted_alphanumeric(data):
     return sorted(data, key=alphanum_key)
 
 
-def print_file(c_dir, depth, write2):
+def print_file(c_dir, depth, sidebar_file, readme_file):
     for f in sorted_alphanumeric(os.listdir(c_dir)):
         re_f = os.path.join(c_dir, f)
         is_d = os.path.isdir(re_f)
         if is_d and f not in ignore_files:
-            write2.write("\t" * depth + "- " + f + "\n")
-            print_file(re_f, depth + 1, write2)
+            content = "\t" * depth + "- " + f + "\n"
+            sidebar_file.write(content)
+            readme_file.write(content)
+            print_file(re_f, depth + 1, sidebar_file, readme_file)
         else:
             if suffix in f and f not in ignore_files:
                 f_n = os.path.splitext(f)[0]
-                write2.write("\t" * depth + "- [" + f_n + "](/" + re_f[2:] + ")\n")
+                content = "\t" * depth + "- [" + f_n + "](/" + re_f[2:] + ")\n"
+                sidebar_file.write(content)
+                readme_file.write(content)
 
 
 os.chdir(doc_path)
 copyfile("../" + read_me_file, read_me_file)
 copyfile("../" + cname_file, cname_file)
+
+# 为README.md生成目录；生成左侧sidebar(summary.md)
 g_f = codecs.open(gen_file_name, 'w', encoding='utf-8')
-print_file(".", 0, g_f)
+r_f = codecs.open(read_me_file, 'a', encoding='utf-8')
+r_f.write("\n## 目录")
+print_file(".", 0, g_f, r_f)
 g_f.close()
+r_f.close()
