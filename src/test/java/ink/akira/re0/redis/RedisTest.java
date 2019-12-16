cf625a4e88e8d80ab03af4d8d4033eec931dc5ec
akira <chin.kou.akira@gmail.com>
2019-12-16 18:01:56 +0800

    pipelining

--- src/test/java/ink/akira/re0/redis/RedisTest.java
+++ src/test/java/ink/akira/re0/redis/RedisTest.java
@@ -2,6 +2,7 @@ package ink.akira.re0.redis;
 
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 
 public class RedisTest {
     @Test
@@ -9,6 +10,21 @@ public class RedisTest {
         try (Jedis jedis = new Jedis("localhost", 6379)) {
             long count = (long) jedis.eval("return #redis.call('keys', ARGV[1])", 0, "blog-read-count:*");
             System.out.println(count);
+        }
+    }
+
+    @Test
+    public void testMulti(){
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+            Pipeline pipelined = jedis.pipelined();
+            pipelined.multi();
+            String teacherId = "1143013021";
+            String courseId = "en-118";
+            pipelined.sadd("teacher-id", teacherId);
+            pipelined.sadd("course-id", courseId);
+            pipelined.sadd("teacher-course", teacherId, courseId);
+            pipelined.exec();
+            pipelined.sync();
         }
     }
 }
